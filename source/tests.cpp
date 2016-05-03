e3aad802cbd274cd5da3f7b03186749589df907c
annachiu7 <qujiani7879@126.com>
2016-05-03 18:50:03 +0200

    Update tests for Vec2

--- source/tests.cpp
+++ source/tests.cpp
@@ -46,6 +46,44 @@ TEST_CASE("describe_vec2_andoperator","[operator_vec2]")
 	REQUIRE(v6_2.y == Approx(1.0f)); 
 } 
 
+TEST_CASE("describe_vec2_operator","[opr_vec2]") 
+{ 
+	Vec2 v1;
+	Vec2 v2{2.0f, 1.0f};
+	Vec2 v3 = v1 + v2;
+	REQUIRE(v3.x == Approx(2.0f)); 
+	REQUIRE(v3.y == Approx(1.0f)); 
+	Vec2 v4 = v3 + v2;
+	REQUIRE(v4.x == Approx(4.0f)); 
+	REQUIRE(v4.y == Approx(2.0f));
+
+	Vec2 v5 = v1 - v2;
+	REQUIRE(v5.x == Approx(-2.0f)); 
+	REQUIRE(v5.y == Approx(-1.0f)); 
+	Vec2 v6 = v4 - v2;
+	REQUIRE(v6.x == Approx(2.0f)); 
+	REQUIRE(v6.y == Approx(1.0f)); 
+
+	float s = 3.0f;
+	Vec2 v7 = v1 * s;
+	REQUIRE(v7.x == Approx(0.0f)); 
+	REQUIRE(v7.y == Approx(0.0f)); 
+	Vec2 v8 = v2 * s;
+	REQUIRE(v8.x == Approx(6.0f)); 
+	REQUIRE(v8.y == Approx(3.0f)); 
+
+	Vec2 v9 = v1 / s;
+	REQUIRE(v9.x == Approx(0.0f)); 
+	REQUIRE(v9.y == Approx(0.0f)); 
+	Vec2 v10 = v8 / s;
+	REQUIRE(v10.x == Approx(2.0f)); 
+	REQUIRE(v10.y == Approx(1.0f)); 
+
+	Vec2 v11 = s * v1;
+	REQUIRE(v11.x == Approx(0.0f)); 
+	REQUIRE(v11.y == Approx(0.0f)); 
+}
+
 int main(int argc, char *argv[])
 {
   return Catch::Session().run(argc, argv);
