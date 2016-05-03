0b6b1f1775843661b7bad21c828ba9e71ee39d18
annachiu7 <qujiani7879@126.com>
2016-05-03 18:47:27 +0200

    Update Vec2

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -11,6 +11,8 @@ x{a},
 y{b}
 {}
 
+////////////////////////////////////////////////////////////
+
 Vec2& Vec2::operator+=(Vec2 const& v) 
 {
 	x += v.x;
@@ -37,4 +39,31 @@ Vec2& Vec2::operator/=(float v)
 	x /= v;
 	y /= v;
 	return *this;
+}
+
+///////////////////////////////////////////////////////////
+
+Vec2 operator+(Vec2 const& u, Vec2 const& v)
+{
+	return Vec2{u.x + v.x, u.y + v.y};
+}
+
+Vec2 operator-(Vec2 const& u, Vec2 const& v)
+{
+	return Vec2{u.x - v.x, u.y - v.y};
+}
+
+Vec2 operator*(Vec2 const& v, float s)
+{
+	return Vec2{v.x * s, v.y * s};
+}
+
+Vec2 operator/(Vec2 const& v, float s)
+{
+	return Vec2{v.x / s, v.y / s};
+}
+
+Vec2 operator*(float s, Vec2 const& v)
+{
+	return Vec2{v.x * s, v.y * s};
 }
--- source/vec2.hpp
+++ source/vec2.hpp
@@ -1,10 +1,11 @@
 #ifndef VEC2_HPP
 #define VEC2_HPP
 
+// Vec2 class definition
 class Vec2
 {
 public:
-	Vec2();
+	Vec2();  // default constructor
 	Vec2(float a, float b);
 
 	float x;
@@ -15,8 +16,14 @@ public:
 	Vec2& operator*=(float v);
 	Vec2& operator/=(float v);
 
+
 };
 
 
+Vec2 operator+(Vec2 const& u, Vec2 const& v);
+Vec2 operator-(Vec2 const& u, Vec2 const& v);
+Vec2 operator*(Vec2 const& v, float s);
+Vec2 operator/(Vec2 const& v, float s);
+Vec2 operator*(float s, Vec2 const& v);
 
-#endif
+#endif // VEC2_HPP
