504a4526e1e71fb4344777c3e4a26cf7571a0d20
annachiu7 <qujiani7879@126.com>
2016-05-04 18:35:13 +0200

    Update Mat2_2.6

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -1,28 +1,33 @@
 #include "mat2.hpp"
 #include <iostream>
+#include <math.h>
 
 Mat2::Mat2() :
-a1{1},
-a2{0},
-b1{0},
-b2{1}
+a{1},
+b{0},
+c{0},
+d{1}
 {}
 
-Mat2::Mat2(int a, int b, int c, int d) :
-a1{a},
-a2{b},
-b1{c},
-b2{d}
+Mat2::Mat2(float a1, float a2, float b1, float b2) :
+a{a1},
+b{a2},
+c{b1},
+d{b2}
 {}
 
 /////////////////////////////////////////////////////////
 
 Mat2& Mat2::operator *=( Mat2 const & m )
 {
-	a1 = a1*m.a1+a2*m.b1;
-	a2 = a1*m.a2+a2*m.b2;
-	b1 = b1*m.a1+b2*m.b1;
-	b2 = b1*m.a2+b2*m.b2;
+	float i = a * m.a + b * m.c;
+	float j = a * m.b + b * m.d;
+	float k = c * m.a + d * m.c;
+	float l = c * m.b + d * m.d;
+	a = i;
+	b = j;
+	c = k;
+	d = l;
 	return *this;
 }
 
@@ -30,8 +35,58 @@ Mat2& Mat2::operator *=( Mat2 const & m )
 
 Mat2 operator *( Mat2 const & m1 , Mat2 const & m2 )
 {
-	return Mat2{m1.a1*m2.a1+m1.a2*m2.b1, 
-				m1.a1*m2.a2+m1.a2*m2.b2, 
-				m1.b1*m2.a1+m1.b2*m2.b1,
-				m1.b1*m2.a2+m1.b2*m2.b2};
+	return Mat2{m1.a * m2.a + m1.b * m2.c, 
+				m1.a * m2.b + m1.b * m2.d, 
+				m1.c * m2.a + m1.d * m2.c,
+				m1.c * m2.b + m1.d * m2.d};
+}
+
+////////////////////////////////////////////////////////
+/*
+float Mat2::det()
+{
+	return a*d - b*c;
+}
+*/
+Vec2 operator *( Mat2 const & m , Vec2 const & v )
+{
+	return Vec2{m.a * v.x + m.b * v.y,
+				m.c * v.x + m.d * v.y};
+}
+
+Vec2 operator *( Vec2 const & v , Mat2 const & m )
+{
+	return Vec2{v.x * m.a + v.y * m.c,
+				v.x * m.b + v.y * m.d};
+}
+
+Mat2 inverse(Mat2 const& m)
+{
+
+	if(m.a * m.d - m.b * m.c == 0)
+	{
+		return Mat2{0,0,0,0};
+	}
+	else	
+	{	
+		float a = m.d / ( m.a * m.d - m.b * m.c );
+		float b = -m.b / ( m.a * m.d - m.b * m.c );
+		float c = -m.c / ( m.a * m.d - m.b * m.c );
+		float d = m.a / ( m.a * m.d - m.b * m.c );
+		return Mat2{a,b,c,d};
+	}
+}
+
+Mat2 transpose(Mat2 const& m)
+{
+	return Mat2{m.a,m.c,m.b,m.d};
+}
+
+Mat2 make_rotation_mat2(float phi)
+{
+	float a = cos(phi);
+	float b = -sin(phi);
+	float c = sin(phi);
+	float d = cos(phi);
+	return Mat2{a,b,c,d};
 }
