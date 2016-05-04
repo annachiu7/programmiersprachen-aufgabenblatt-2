285dda8dc85044984eec06e02c9291aed1937aad
annachiu7 <qujiani7879@126.com>
2016-05-04 09:43:18 +0200

    Update tests

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 #include "vec2.hpp"
+#include "mat2.hpp"
 
 TEST_CASE("describe_vec2","[create_vec2]") 
 { 
@@ -83,6 +84,75 @@ TEST_CASE("describe_vec2_operator","[opr_vec2]")
 	REQUIRE(v11.x == Approx(0.0f)); 
 	REQUIRE(v11.y == Approx(0.0f)); 
 }
+
+TEST_CASE("describe_mat2","[create_mat2]") 
+{ 
+	Mat2 m_einheit;
+	Mat2 m1{1,1,1,1};
+	Mat2 m2{1,2,3,4};
+	REQUIRE(m1.a1 == 1); 
+	REQUIRE(m1.a2 == 1); 
+	REQUIRE(m1.b1 == 1);
+	REQUIRE(m1.b2 == 1); 
+	REQUIRE(m_einheit.a1 == 1); 
+	REQUIRE(m_einheit.a2 == 0); 
+	REQUIRE(m_einheit.b1 == 0);
+	REQUIRE(m_einheit.b2 == 1); 
+	REQUIRE(m2.a1 == 1); 
+	REQUIRE(m2.a2 == 2); 
+	REQUIRE(m2.b1 == 3);
+	REQUIRE(m2.b2 == 4); 
+}
+
+TEST_CASE("describe_mult_mrf_mat2","[mult_mrf_mat2]") 
+{ 
+	Mat2 m_einheit;
+	Mat2 m1{1,1,1,1};
+	Mat2 m2 = m1*=m_einheit;
+	REQUIRE(m2.a1 == 1); 
+	REQUIRE(m2.a2 == 1); 
+	REQUIRE(m2.b1 == 1); 
+	REQUIRE(m2.b2 == 1); 
+	Mat2 m3 = m_einheit*=m1;
+	REQUIRE(m3.a1 == 1); 
+	REQUIRE(m3.a2 == 1); 
+	REQUIRE(m3.b1 == 1); 
+	REQUIRE(m3.b2 == 1); 
+	Mat2 m4 = m1*=m1;
+	REQUIRE(m4.a1 == 2); 
+	REQUIRE(m4.a2 == 2); 
+	REQUIRE(m4.b1 == 2); 
+	REQUIRE(m4.b2 == 2); 
+}
+
+
+TEST_CASE("describe_mult_mat2","[mult_mat2]") 
+{ 
+	Mat2 m_einheit;
+	Mat2 m1{1,1,1,1};
+	Mat2 m2{1,2,3,4};
+	Mat2 m3 = m_einheit * m1;
+	REQUIRE(m3.a1 == 1); 
+	REQUIRE(m3.a2 == 1); 
+	REQUIRE(m3.b1 == 1); 
+	REQUIRE(m3.b2 == 1); 
+	Mat2 m4 = m_einheit * m2;
+	REQUIRE(m4.a1 == 1); 
+	REQUIRE(m4.a2 == 2); 
+	REQUIRE(m4.b1 == 3); 
+	REQUIRE(m4.b2 == 4); 
+	Mat2 m5 = m1 * m2;
+	REQUIRE(m5.a1 == 4); 
+	REQUIRE(m5.a2 == 6); 
+	REQUIRE(m5.b1 == 4); 
+	REQUIRE(m5.b2 == 6); 
+	Mat2 m6 = m2 * m1;
+	REQUIRE(m6.a1 == 3); 
+	REQUIRE(m6.a2 == 3); 
+	REQUIRE(m6.b1 == 7); 
+	REQUIRE(m6.b2 == 7); 
+}
+
 
 int main(int argc, char *argv[])
 {
