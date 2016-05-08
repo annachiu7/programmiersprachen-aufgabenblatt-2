c77ce214b646e2320eb87a83b4b69a8973a327c4
annachiu7 <qujiani7879@126.com>
2016-05-09 00:53:57 +0200

    Update example.cpp 2.14

--- source/example.cpp
+++ source/example.cpp
@@ -17,18 +17,22 @@ int main(int argc, char* argv[])
     }
 
     auto t = win.get_time();
-    float x1{0.5f + 0.5f * std::sin(t)};
-    float y1{0.5f + 0.5f * std::cos(t)};
+    float x1{0.5f + 0.4f * std::sin(t*M_PI/30)};
+    float y1{0.5f + 0.4f * std::cos(t*M_PI/30)};
 
-    float x2{0.5f + 0.5f * std::sin(2.0f*t)};
-    float y2{0.5f + 0.5f * std::cos(2.0f*t)};
+    float x2{0.5f + 0.4f * std::sin(t*M_PI/30/60)};
+    float y2{0.5f + 0.4f * std::cos(t*M_PI/30/60)};
 
-    float x3{0.5f + 0.5f * std::sin(t-10.f)};
-    float y3{0.5f + 0.5f * std::cos(t-10.f)};
+    float x3{0.5f + 0.4f * std::sin(t*M_PI/30/60/60)};
+    float y3{0.5f + 0.4f * std::cos(t*M_PI/30/60/60)};
 
     win.draw_point(x1, y1, 1.0f, 0.0f, 0.0f);
     win.draw_point(x2, y2, 0.0f, 1.0f, 0.0f);
     win.draw_point(x3, y3, 0.0f, 0.0f, 1.0f);
+
+    win.draw_line(0.5f, 0.5f, x1, y1, 1, 0, 0);
+    win.draw_line(0.5f, 0.5f, x2, y2, 0, 1, 0);
+    win.draw_line(0.5f, 0.5f, x3, y3, 0, 0, 1);
 
     auto m = win.mouse_position();
     //win.draw_line(0.1f, 0.1f, 0.8f,0.1f, 1.0,0.0,0.0);
@@ -44,17 +48,17 @@ int main(int argc, char* argv[])
     mp.x = m.first;
     mp.y = m.second;
     // create rec
-    Rectangle rec{0.1,0.1,0.3f,0.2f,{0.0,0.0,0.0}};
-    rec.draw(win, rec.get_Color());
-    if (rec.is_inside(mp)  ==  true)
-    {
-      rec.draw(win, {0.0,0.0,1.0});
-    }
-    else{
-      rec.draw(win, rec.get_Color());
-    }
+//    Rectangle rec{0.1,0.1,0.3f,0.2f,{0.0,0.0,0.0}};
+//    rec.draw(win, rec.get_Color());
+//    if (rec.is_inside(mp)  ==  true)
+//    {
+//      rec.draw(win, {0.0,0.0,1.0});
+//    }
+//    else{
+//      rec.draw(win, rec.get_Color());
+//    }
     //create circle
-    Circle c{0.5,0.5,0.5,{0.0}};
+    Circle c{0.5,0.5,0.43,{0.0}};
     c.draw(win, {0.0});
     if (c.is_inside(mp) == true)
     {
